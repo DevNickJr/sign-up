6f35e23d59a234ead5c2615489c331467a9e6f71
Nicholasjr <nicholasduadei14@gmail.com>
2022-04-01 23:45:36 +0100

    updated the error alert to instead use innerhtml

--- main.js
+++ main.js
@@ -1,5 +1,48 @@
+function validation() {
+    let form = document.forms[0];
+    let pattern = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    let email = document.getElementById('email').value;
+    let check= true;
+    if (form[0].value.trim() == '') {
+        form[0].className = 'error';  
+        document.getElementById('fname-p').innerHTML = `${form[0].placeholder} cannot be empty`;       
+        check = false;
+        }
+    if (form[1].value.trim() == '') {
+        form[1].className = 'error';
+        document.getElementById('lname-p').innerHTML = `${form[1].placeholder} cannot be empty`;    
+        check = false;
+        }
+    if (form[2].value.trim() == '') {
+        form[2].className = 'error';   
+        document.getElementById('email-p').innerHTML = `${form[2].placeholder} cannot be empty`;        
+        check = false;
+        } else if (!pattern.test(email)) {
+            form[2].className = 'error';  
+            document.getElementById('email-p').innerHTML = `Looks like this is not an email`;
+            check=  false;
+        } 
+
+    if (form[3].value.trim() == '') {
+        document.getElementById('pword-p').innerHTML = `${form[3].placeholder} cannot be empty`;  
+        form[3].className = 'error';            
+        check = false;
+        }    
+    return check
+}
 
 
+// var inputFocusEvent = document.getElementsByTagName('input');
+// for (var i of inputFocusEvent) {
+//     i.addEventListener('click', function() {        
+//         i.className = 'error stop';
+//         document.getElementsByClassName(`${i.className}`)[0].value= 'meeeeeeee'
+//     }, false);
+// }
+
+
+
+//initial code
 // function validation() {
 //     let form = document.forms[0];
 //     let pattern = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
@@ -28,53 +71,3 @@
 
 
 
-
-function validation() {
-    let form = document.forms[0];
-    let pattern = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    let email = document.getElementById('email').value;
-    let check= true;
-    if (form[0].value.trim() == '') {
-        alert(`${form[0].placeholder} cannot be empty`);
-        form[0].className = 'error';            
-        alert(form[0].className);
-        check = false;
-        }
-    if (form[1].value.trim() == '') {
-        alert(`${form[1].placeholder} cannot be empty`);
-        form[1].className = 'error';            
-        alert(form[1].className);
-        check = false;
-        }
-    if (form[2].value.trim() == '') {
-        alert(`${form[2].placeholder} cannot be empty`);
-        form[2].className = 'error';            
-        alert(form[2].className);
-        check = false;
-        } else if (!pattern.test(email)) {
-            alert(pattern.test(email));
-            alert('Looks like this is not an email');
-            form[2].className = 'error';  
-            check=  false;
-        }
-
-    if (form[3].value.trim() == '') {
-        alert(`${form[3].placeholder} cannot be empty`);
-        form[3].className = 'error';            
-        alert(form[3].className);
-        check = false;
-        }    
-    return check
-}
-
-
-// var inputFocusEvent = document.getElementsByTagName('input');
-// for (var i of inputFocusEvent) {
-//     i.addEventListener('focus', focus() {
-//         alert('red');
-//     }, false);
-// }
-
-// document.getElementById('fname').addEventListener('focus', focus() {
-//     alert('green')
-// })
